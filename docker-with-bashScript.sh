3b0b5d5e81fc2feaf09397c9d7f85fd531034b3c
mehran pornour <mehran.p51@gmail.com>
2024-03-26 09:04:27 +0000

    bash script to install docker

--- docker-with-bashScript.sh
+++ docker-with-bashScript.sh
@@ -12,8 +12,12 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+
 sudo docker run hello-world
+
 # Linux post-install
+
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
+
